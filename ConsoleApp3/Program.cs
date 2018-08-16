c6ac124446f5b885a5bbc42bb4dc28b0a73f09d3
김병철 <peppi@outlook.kr>
2018-08-17 03:24:41 +0900

    동기화 공부2

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp3
+{
+    class ThisLock
+    {
+        public void IncreaseCount(ref int count)
+        {
+            count++;
+        }
+    }
+    class Test
+    {
+        ThisLock lockObject = new ThisLock();
+        int Count = 0;
+
+        public void ThreadProc()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                lockObject.IncreaseCount(ref Count);
+                Console.WriteLine("Thread ID: {0} Copunt:{1}", Thread.CurrentThread.GetHashCode(), Count);
+
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            Thread[] threads = new Thread[3];
+            for(int i=0; i < 3; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(test.ThreadProc));
+            }
+            for(int i =0; i < 3; i++)
+            {
+                threads[i].Start();
+            }
+        }
+    }
+}
